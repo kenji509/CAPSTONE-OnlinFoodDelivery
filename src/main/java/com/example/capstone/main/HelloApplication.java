d0fe7bb7f0c7106d075e335096093add325d9830
Kenji Uehara <kenjiuehara509@gmail.com>
2026-07-20 19:56:18 +0800

    Add session serialization, SOLID principles (SRP, DIP), and updated README

--- src/main/java/com/example/capstone/main/HelloApplication.java
+++ src/main/java/com/example/capstone/main/HelloApplication.java
@@ -1,5 +1,10 @@
 package com.example.capstone.main;
 
+import com.example.capstone.controller.MenuController;
+import com.example.capstone.model.Customer;
+import com.example.capstone.model.Rider;
+import com.example.capstone.model.User;
+import com.example.capstone.util.SessionManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,11 +15,33 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(
-                HelloApplication.class.getResource("/com/example/capstone/login-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Login");
-        stage.setScene(scene);
+
+        User existingUser = SessionManager.loadSession();
+
+        if (existingUser instanceof Customer customer) {
+            FXMLLoader loader = new FXMLLoader(
+                    HelloApplication.class.getResource("/com/example/capstone/menu-view.fxml"));
+            Scene scene = new Scene(loader.load());
+            MenuController menuController = loader.getController();
+            menuController.setCustomer(customer);
+            stage.setTitle("Menu");
+            stage.setScene(scene);
+
+        } else if (existingUser instanceof Rider) {
+            FXMLLoader loader = new FXMLLoader(
+                    HelloApplication.class.getResource("/com/example/capstone/rider-dashboard-view.fxml"));
+            Scene scene = new Scene(loader.load());
+            stage.setTitle("Rider Dashboard");
+            stage.setScene(scene);
+
+        } else {
+            FXMLLoader loader = new FXMLLoader(
+                    HelloApplication.class.getResource("/com/example/capstone/login-view.fxml"));
+            Scene scene = new Scene(loader.load());
+            stage.setTitle("Login");
+            stage.setScene(scene);
+        }
+
         stage.show();
     }
 
